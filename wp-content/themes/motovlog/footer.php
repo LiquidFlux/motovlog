5cf8cb5ecc9a14c5689dae42b806c49e2e806bd1
LiquidFlux <me@charlescarnell.co.uk>
2014-02-22 09:40:03 +0000

    Initial commit

---
+++ wp-content/themes/motovlog/footer.php
@@ -0,0 +1,21 @@
+    <footer>
+        <div class="container">
+            <hr />
+            <div class="footerText">
+                Design by Charles Carnell.
+            </div>
+        </div>
+    </footer>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="/custom/scripts/bootstrap.js"></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-40798833-1', 'liquidflux.co.uk');
+        ga('send', 'pageview');
+    </script>
+</body>
+</html>
---
+++ wp-content/themes/motovlog/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php wp_title(''); ?> :: LiquidFlux</title>
+	<meta name="description" content="LiquidFlux Web Design & Development">
+	<meta name="keywords" content="Liquid, Flux, LiquidFlux, webdev, design, development">
+	<meta name="author" content="Charles Carnell">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="/custom/css/bootstrap.css" rel="stylesheet">
+	<link href="/custom/css/site.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<header>
+		<div class="container clearfix">
+			<div class="pull-left logo">
+				<a href="/">
+					<img src="/custom/images/logo.png" alt="LiquidFlux Web Design &amp; Development">
+				</a>
+			</div>
+			<nav class="pull-right">
+				<ul>
+					<li><a href="/authors/" data-title="Authors">Authors</a></li>
+					<li><a href="/contact/" data-title="Contact">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
